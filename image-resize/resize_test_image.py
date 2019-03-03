4672f37d651ca6277ba79d9caaf17b6a081a4281
Pavan Kurapati <pavan.kurapati@gmail.com>
2019-03-03 16:25:50 -0500

    Test image resize

---
+++ image-resize/resize_test_image.py
@@ -0,0 +1,44 @@
+import cv2
+from keras.preprocessing.image import img_to_array
+import imutils
+
+def preprocess(image,scale=128):
+    """ preprocess the test image and covert to array """
+    inter=cv2.INTER_AREA
+    (h, w) = image.shape[:2]
+    dW = 0
+    dH = 0
+    
+    width = scale
+    height = scale
+
+    # if the width is smaller than the height, then resize
+    # along the width (i.e., the smaller dimension) and then
+    # update the deltas to crop the height to the desired
+    # dimension
+    if w < h:
+        image = imutils.resize(image, width=width,
+            inter=inter)
+        dH = int((image.shape[0] - height) / 2.0)
+
+    # otherwise, the height is smaller than the width so
+    # resize along the height and then update the deltas
+    # crop along the width
+    else:
+        image = imutils.resize(image, height=height,
+            inter=inter)
+        dW = int((image.shape[1] - width) / 2.0)
+
+    # now that our images have been resized, we need to
+    # re-grab the width and height, followed by performing
+    # the crop
+    (h, w) = image.shape[:2]
+    image = image[dH:h - dH, dW:w - dW]
+
+    # finally, resize the image to the provided spatial
+    # dimensions to ensure our output image is always a fixed
+    # size
+    
+    image =  cv2.resize(image, (width, height),
+        interpolation=inter)
+    return img_to_array(image)
